d5b2e6d78a158cc0a02c915304cab1235b905df5
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-05-08 18:31:48 +0530

    implement login verification

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -20,3 +20,44 @@ export const createUserController = async (req, res)=>{
     }
 
 }
+
+export const loginController = async (req, res)=>{
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                errors: 'email not registered'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                errors: "Invalid password"
+            })
+        }
+
+        const token = await user.generateJWT();
+        res.status(200).json({user, token});
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+}
+
+export const profileController = async (req, res)=>{
+    console.log(req.user);
+    res.status(200).json({
+        user: req.user
+    });
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as userController from '../controllers/user.controller.js'
 import { body } from "express-validator";
+import * as authMiddleware from '../middlewares/auth.middleware.js'
 
 const router = Router();
 
@@ -8,5 +9,13 @@ router.post('/register',
     body('email').isEmail().withMessage("Email must be a valid email address"),
     body('password').isLength({min: 8}).withMessage("Password must be atleast 8 characters long"),
     userController.createUserController);
+
+router.post('/login',
+    body('email').isEmail().withMessage("Email must be a valid email address"),
+    body('password').isLength({min: 8}).withMessage("Password must be atleast 8 characters long"),
+    userController.loginController);
+
+router.get('/profile', authMiddleware.authUser, userController.profileController);
+
 
 export default router;
