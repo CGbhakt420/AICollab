7e0fbb9cb7c008d8ab5ba6991e0caabff8b1c46d
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-05-09 19:39:52 +0530

    done user auth in backend and basic frontend design for login and register

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../db/models/user.model.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+import redisClient from "../services/redis.service.js";
 
 export const createUserController = async (req, res)=>{
     const errors = validationResult(req);
@@ -13,6 +14,7 @@ export const createUserController = async (req, res)=>{
         const user = await userService.createUser(req.body);
 
         const token = await user.generateJWT();
+        delete user._doc.password;  //so that password is not sent in the response and doesnt show up in console
         res.status(201).json({ user, token });
     }
     catch(error){
@@ -23,7 +25,7 @@ export const createUserController = async (req, res)=>{
 
 export const loginController = async (req, res)=>{
     const errors = validationResult(req);
-
+    //check for any errors in the input field validation by express-validator
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()});
     }
@@ -47,6 +49,7 @@ export const loginController = async (req, res)=>{
         }
 
         const token = await user.generateJWT();
+        delete user._doc.password;
         res.status(200).json({user, token});
     }
     catch(err){
@@ -60,4 +63,19 @@ export const profileController = async (req, res)=>{
     res.status(200).json({
         user: req.user
     });
+}
+
+export const logoutController = async (req, res)=>{
+    try{
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+        redisClient.set(token, 'logout', 'EX', 60*60*24); //ex = expiration after 24hrs
+        res.status(200).json({
+            message: 'Logged out succesfully',
+        });
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err.message);
+    }
 }
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv'
 dotenv.config();
 
+import redisClient from '../services/redis.service.js';
+
 export const authUser = async (req, res, next)=>{
     try{
         const token = req.cookies.token || req.headers.authorization.split(' ')[1];
@@ -9,6 +11,12 @@ export const authUser = async (req, res, next)=>{
             return res.status(401).send({error: 'Unauthorized User'});
         }
 
+        const tokenBlacklist = await redisClient.get(token); //token mil jaata hai yaani blacklisted hai aur agar nahi milta hai yaani user ne logout nahi kiya hai
+        //logged out token ko redis me store kardenge, because jwt is stateless and it will be valid until it is expired even afte logout
+        if(tokenBlacklist){
+            res.cookie('token', '');
+            return res.status(401).send({error: 'Unauthorized User'})
+        }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
