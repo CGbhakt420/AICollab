46fdc7ca325b9db66595a5c52fb43b6bb3af4e33
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-06-25 23:16:26 +0530

    minor fix

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -103,4 +101,9 @@ io.on('connection', (socket) => {
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
